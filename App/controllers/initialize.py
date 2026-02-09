a5d4f386a7c47775e9c852a9a6db100ee437d2fb
Aioblitz <60820112+Aioblitz@users.noreply.github.com>
2026-02-09 11:54:54 -0400

    Updated Admin & User
    Fixed the error with initialisation

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -1,8 +1,9 @@
-from .user import create_user
+from .user import create_user, create_admin
 from App.database import db
 
 
 def initialize():
     db.drop_all()
     db.create_all()
-    create_user('bob', 'bobpass')
+    create_user('bob', 'bobpass','bob@mail.com')
+    create_admin('admin', 'adminpass', 'admin@mail.com')
