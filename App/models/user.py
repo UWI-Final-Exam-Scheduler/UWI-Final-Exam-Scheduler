7181c1bd7d01b37747559a5e7b588ebd4f1e575d
Aioblitz <60820112+Aioblitz@users.noreply.github.com>
2026-02-09 11:45:55 -0400

    Added Admin
    Updated User and created Admin

--- App/models/user.py
+++ App/models/user.py
@@ -2,18 +2,29 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String(20), nullable=False, unique=True)
+    username =  db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    type = db.Column(db.String(50))
 
-    def __init__(self, username, password):
+    __mapper_args__ = {
+        'polymorphic_identity': 'user',
+        'polymorphic_on': type
+    }
+
+    
+    def __init__(self, username, password, email):
         self.username = username
         self.set_password(password)
+        self.email = email
 
     def get_json(self):
         return{
             'id': self.id,
-            'username': self.username
+            'username': self.username,
+            'email': self.email
         }
 
     def set_password(self, password):
@@ -23,4 +34,5 @@ class User(db.Model):
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
+        
 
