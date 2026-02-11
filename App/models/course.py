6719ef90adc6cbbb519ce33530ae0caeda33c3a8
Jamal Mohammed <jamalm2427@gmail.com>
2026-02-11 18:34:48 -0400

    model change

--- App/models/course.py
+++ App/models/course.py
@@ -4,7 +4,7 @@ class Courses(db.Model):
     __tablename__ = "courses"
 
     id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False, unique=True)
     
     def __init__(self, name):
         self.name = name
