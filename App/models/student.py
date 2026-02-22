0dfc97807a7ebcfcbfc9d151b266506493138fd9
Jamal Mohammed <jamalm2427@gmail.com>
2026-02-22 19:35:37 -0400

    database reset for PK model change

--- App/models/student.py
+++ App/models/student.py
@@ -3,11 +3,10 @@ from App.database import db
 class Student(db.Model):
     __tablename__='students'
     
-    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
-    student_id = db.Column(db.Integer, nullable=False, unique=True)
+    student_id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
     
     enrollments = db.relationship('Enrollment', back_populates='student')
 
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, student_id):
+        self.student_id = student_id
     
--- App/models/venue.py
+++ App/models/venue.py
@@ -2,6 +2,7 @@ from App.database import db
 
 class Venue(db.Model):
     __tablename__ = 'venues'
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False, unique=True)
     capacity = db.Column(db.Integer, nullable=False)
