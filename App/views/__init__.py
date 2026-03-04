cc66f5eb33739099b37a8181d8244a6a414d9bdb
jamie07262 <franchesca.james@my.uwi.edu>
2026-03-04 01:16:03 -0400

    update venues controller and views
    Controller
    - add get_all_venues
    - add get_venue_by_name
    
    View
    - /api/venues
    -/api/venues/<venue_name>

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -5,7 +5,7 @@ from .index import index_views
 from .auth import auth_views
 from .admin import setup_admin
 from .courses import course_views
+from .venues import venue_views
 
-
-views = [user_views, index_views, auth_views, course_views] 
+views = [user_views, index_views, auth_views, course_views, venue_views] 
 # blueprints must be added to this list
---
+++ App/views/venues.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
+from App.controllers import venue
+from App.models.admin import Admin
+from flask_jwt_extended import jwt_required, current_user as jwt_current_user, get_jwt_identity
+
+from.index import index_views
+
+from App.controllers.venue import (
+    get_all_venues,
+    get_venue_by_name,
+)
+from App.controllers.auth import is_admin
+
+venue_views = Blueprint('venue_views', __name__, template_folder='../templates')
+
+@venue_views.route('/api/venues', methods=['GET'])
+@jwt_required()
+def get_venues_endpoint():
+    authenticated_user = get_jwt_identity()
+
+    # Ensure the user is authenticated before accessing venues
+    if not is_admin(authenticated_user):
+        return jsonify({'error': 'Access denied - Unauthorized user'}), 401
+        
+    try:
+        venues = get_all_venues()
+        if venues is None:
+            return jsonify({'error': 'No venues found'}), 404
+        return jsonify(venues), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+@venue_views.route('/api/venues/<venue_name>', methods=['GET'])
+@jwt_required()
+def get_venue_by_name_endpoint(venue_name):
+    authenticated_user = get_jwt_identity()
+
+    # Ensure the user is authenticated before accessing venues
+    if not is_admin(authenticated_user):
+        return jsonify({'error': 'Access denied - Unauthorized user'}), 401
+        
+    try:
+        venue = get_venue_by_name(venue_name)
+        if isinstance(venue, str):
+            return jsonify({'error': venue}), 404
+        return jsonify(venue), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
