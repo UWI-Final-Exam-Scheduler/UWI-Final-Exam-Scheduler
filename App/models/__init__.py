19b390677efebe006adc5a8f5247d19702e3f7b8
Jamal Mohammed <jamalm2427@gmail.com>
2026-03-10 23:03:21 -0400

    created the Exam Model for Scheduling Exams

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -5,3 +5,4 @@ from .student import *
 from .enrollment import *
 from .admin import *
 from .clash_matrix import *
+from .exam import *
---
+++ App/models/exam.py
@@ -0,0 +1,23 @@
+from App.database import db
+
+class Exam(db.Model):
+    __tablename__ = "exams"
+
+    id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True)
+    courseCode= db.Column(db.String(20), db.ForeignKey('courses.courseCode'), nullable=False)
+    date = db.Column(db.Date, nullable=False, index=True)
+    time = db.Column(db.Time, nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False, index=True)
+    exam_length = db.Column(db.Integer, nullable=False)
+    number_of_students = db.Column(db.Integer, nullable=False)
+    
+    course = db.relationship('Course', back_populates='exams')
+    venue = db.relationship("Venue", back_populates="exams")
+
+    def __init__(self, courseCode, date, time, venue_id, exam_length, number_of_students):
+        self.courseCode = courseCode
+        self.date = date
+        self.time = time
+        self.venue_id = venue_id
+        self.exam_length = exam_length
+        self.number_of_students = number_of_students
