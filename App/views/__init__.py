a0304697badd631c20d0030b8729c66a68c6e062
jamie07262 <franchesca.james@my.uwi.edu>
2026-03-19 09:39:18 -0400

    add clash matrix views
    - update courses
    - update clash matrix controllers
    - update wsgi

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -1,12 +1,11 @@
-# blue prints are imported 
-# explicitly instead of using *
-from .user import user_views
-from .index import index_views
-from .auth import auth_views
 from .admin import setup_admin
 from .courses import course_views
 from .venues import venue_views
 from .enrollments import enrollment_views
+from .clash_matrix import clash_matrix_views
+from .index import index_views
+from .auth import auth_views
+from .user import user_views
 
-views = [user_views, index_views, auth_views, course_views, venue_views, enrollment_views] 
+views = [user_views, index_views, auth_views, course_views, venue_views, enrollment_views, clash_matrix_views] 
 # blueprints must be added to this list
